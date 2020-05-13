8d5cb72fb3269fd7fd76b87f1748f5d6892d200c
Manan Arora <31929934+manan0313@users.noreply.github.com>
2020-05-13 06:50:52 +0530

    Fixed: Layout and clock were intersecting

--- app/src/main/java/com/example/ezstopwatch/MainActivity.java
+++ app/src/main/java/com/example/ezstopwatch/MainActivity.java
@@ -82,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 int hours = seconds / 3600;
                 int minutes = (seconds % 3600) / 60;
                 int secs = (seconds % 60);
+
                 String time = String.format("%d:%02d:%02d", hours, minutes, secs);
                 tvTimer.setText(time);
 
